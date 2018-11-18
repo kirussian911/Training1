3b6e52fbc0f4ace99dc23828d94385ab31ab9371
kirussian911 <34275849+kirussian911@users.noreply.github.com>
2018-11-18 04:57:53 +0400

    Тестовая выгрузка из Pycharm

---
+++ venv/for_test.py
@@ -0,0 +1,30 @@
+# from datetime import datetime as dt
+#
+# now = dt.now()
+# print(f'Текущее время {now:%d.%m.%Y %H.%M}')
+
+# x = 10
+# y = 5
+# Ответ: 10 x 5 / 2 = 25.0
+
+# x, y = 10, 5
+# print(f'x * y / 2 = {x * y / 2}')
+# print(f"{x} x {y} / 2 = {x * y / 2}")
+
+# name = "Дмитрий"
+# age = 25
+#
+# print(f'Меня зовут {name}. Мне {age} лет.')
+
+
+# planets = ["Меркурий", "Венера", "Земля", "Марс"]
+# print(f'Мы живем на планете {planets[2]}')
+
+# planet = {"name": "Земля", "radius": 6378000}
+# print(f'Планета {planet["name"]}.Радиус {planet["radius"]/1000} км.')
+
+# digits = {0: 'ноль', 'one': 'один'}
+# print(f'{digits[0]}-ноль, {digits["one"]} - один')
+
+name ='Дмитрий'
+print(f'{name.upper()}')
